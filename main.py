4fb7bca3e410295253cceeef2604c455415f7de5
Annicheez <60089829+Annicheez@users.noreply.github.com>
2020-08-27 01:00:56 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -4,27 +4,71 @@ import os
 import dotenv
 import nltk
 from utils import *
-
-dotenv.load_dotenv()
-
-connection = psy.connect(host = os.environ['pghost'], port = 5432, database = os.environ['pgdatabase'],
-                            user = os.environ['pgusername'], password = os.environ['pgpassword'])
-sql_query = """SELECT speaker_data.file_name, speaker_data.last_update, call_files.file_name, call_files.mtime, 
-call_files.ctime, company_ids.cusip, speaker_data.speaker_name, speaker_data.role, speaker_data.context, 
-speaker_data.speaker_number,speaker_data.speaker_text
-FROM streetevents.speaker_data
-INNER JOIN streetevents.call_files
-	ON speaker_data.file_name = call_files.file_name
-INNER JOIN streetevents.company_ids 
-	ON speaker_data.file_name = company_ids.file_name
-LIMIT 100
-"""
-
-data = pd.read_sql(sql_query, connection)
-token_data  = tokenise(data.speaker_text)
-ctoken_data = remove_stopwords(token_data)
-tagged_data = pos_tagging(ctoken_data)
-
-print(tagged_data[3])
+import matplotlib.pyplot as plt
 
 
+def main():
+    # Anonymously loading login credentials for the MCCGR database
+    dotenv.load_dotenv()
+
+    # Establishing connection
+    connection = psy.connect(host = os.environ['pghost'], port = 5432, database = os.environ['pgdatabase'],
+                                user = os.environ['pgusername'], password = os.environ['pgpassword'])
+
+    # SQL query to load data
+    sql_query = """SELECT speaker_data.file_name, speaker_data.last_update, call_files.file_name, call_files.mtime, 
+    call_files.ctime, company_ids.cusip, speaker_data.speaker_name, speaker_data.role, speaker_data.context, 
+    speaker_data.speaker_number,speaker_data.speaker_text
+    FROM streetevents.speaker_data
+    INNER JOIN streetevents.call_files
+        ON speaker_data.file_name = call_files.file_name
+    INNER JOIN streetevents.company_ids 
+        ON speaker_data.file_name = company_ids.file_name
+    LIMIT 100
+    """
+    # Reading the sql query reply into a pandas dataframe
+    data = pd.read_sql(sql_query, connection)
+
+    # Tokenize data
+    token_data = tokenise(data.speaker_text)
+
+    # Remove stopwords
+    ctoken_data = remove_stopwords(token_data)
+
+    # Lemmatize and filter for pronouns and alphanumerics
+    cltoken_data = filter_alphanumerics(filter_pronouns(lemmatize(ctoken_data)))
+
+    # POS tagging
+    tagged_data = pos_tagger(cltoken_data)
+
+    # Isolating adjectives
+    # Collapsing tagged data into one list of tuples
+    collapsed_tags = [tupp for row in tagged_data for tupp in row]
+
+    # Isolating adjectives
+    adjectives = [word for (word, tag) in collapsed_tags if tag in ('JJ', 'JJR', 'JJS')]
+
+    # Summarising adjectives
+    tags_fd = nltk.FreqDist(adjectives)
+
+    # Understanding usage of adjectives
+    # Converting data to bigrams
+    trigrams = nltk.trigrams(collapsed_tags)
+    predecessors = [a[1] for (a, b, c) in trigrams if c[1] in ('JJ', 'JS', 'JR')]
+    nltk.FreqDist(predecessors)
+
+    # Reinitialising the trigram generator object
+    trigrams = nltk.trigrams(collapsed_tags)
+    successors = [b[0] for (a, b, c) in trigrams if c[1] in ('JJ', 'JS', 'JR')]
+    nltk.FreqDist(successors)
+
+    print(nltk.FreqDist(word for (word, tag) in collapsed_tags).most_common(1000))
+    sents = analyse_sentiment(tagged_data)
+    print(sents)
+    # bigrams = bigram_generate(tagged_data)
+    # adj_predec = [a[1] for (a,b) in bigrams if b[1] in ('JJ', 'JJR', 'JJS')]
+    # print(nltk.FreqDist(adj_predec).most_common())
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import nltk
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
+from nltk.corpus import sentiwordnet as swn
 import pandas as pd
+import re
+
 
 def tokenise(data: pd.Series):
     """This function takes a list of strings as input, tokenises it , and returns a list of
@@ -19,17 +22,54 @@ def remove_stopwords(data:pd.Series):
     """This function takes a pandas series object as input where each entry is a list of tokens
     and removes all stopwords
     :param data:
-    :return:cleaned_column
+    :return: cleaned_data
     """
-    cleaned_column = []
+    cleaned_data = []
     stop_words = set(stopwords.words('english'))
     for row in data:
         filtered_sentence = [word for word in row if not word in stop_words and word.isalnum()]
-        cleaned_column.append(filtered_sentence)
-    return pd.Series(cleaned_column)
+        cleaned_data.append(filtered_sentence)
+    return pd.Series(cleaned_data)
 
 
-def pos_tagging(data:pd.Series):
+def lemmatize(data:pd.Series):
+    """This function take a pandas series as input and lemmatizes each token
+    entry in the series. The Wordnet lemmatizer is utilized.
+    :param data:
+    :return: lemmatized_data
+    """
+    lemmatizer = nltk.WordNetLemmatizer()
+    lemmatized_data = []
+    for row in data:
+        lemmatized_sentence = [lemmatizer.lemmatize(token) for token in row]
+        lemmatized_data.append(lemmatized_sentence)
+    return pd.Series(lemmatized_data)
+
+
+def filter_pronouns(data:pd.Series):
+    """This function takes a pandas series as input and filters for pronouns
+    :param data:
+    :return: filtered_data
+    """
+    filtered_data = []
+    for row in data:
+        filtered_tokens = [token for token in row if len(token) > 3]
+        filtered_data.append(filtered_tokens)
+    return pd.Series(filtered_data)
+
+
+def filter_alphanumerics(data:pd.Series):
+    """This function takes a pandas series as input and filters for alpha
+    :param data:
+    :return: filtered_data
+    """
+    filtered_data = []
+    for row in data:
+        filtered_tokens = [token for token in row if not bool(re.search('[0-9]+', token))]
+        filtered_data.append((filtered_tokens))
+    return pd.Series(filtered_data)
+
+def pos_tagger(data:pd.Series):
     """"This function takes a pandas series as input and tags each word as per its
     part of speech classification
     :param data:
@@ -40,3 +80,50 @@ def pos_tagging(data:pd.Series):
         tagged_sentence = nltk.pos_tag(row)
         tagged_data.append(tagged_sentence)
     return pd.Series(tagged_data)
+
+
+def bigram_generate(data:pd.Series):
+    """This function takes a pandas series as input, collapses it into a list of
+    tokens and generates bigrams
+    :param data:
+    :return: bigrams
+    """
+    collapsed_data = [token for row in data for token in row]
+    bigrams = nltk.bigrams(collapsed_data)
+    return bigrams
+
+
+def analyse_sentiment(data:pd.Series):
+    """This function takes a pandas series as input, and tags each sentence according
+    to its sentiment using sentinet
+    :param data:
+    :return: pd.Series
+    """
+    collapsed_data = [tupp for row in data for tupp in row]
+    sentiment_words = []
+
+    def penn_to_wn(tag):
+        """
+        Convert between the PennTreebank tags to simple Wordnet tags
+        """
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        return None
+
+    for word, pos in collapsed_data:
+        wn_tag = penn_to_wn(pos)
+        synsets = wordnet.synsets(word, pos = wn_tag)
+        try:
+            synset = synsets[0]
+        except: pass
+        swn_synset = swn.senti_synset(synset.name())
+        tupp = (word, pos, swn_synset.pos_score(), swn_synset.neg_score(), swn_synset.obj_score())
+        sentiment_words.append(tupp)
+    return sentiment_words
+
